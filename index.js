480dbbeb4e9d4930e916e037c258555d94ac3766
ryounz <liburaian08@gmail.com>
2025-01-25 13:45:36 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+  const dogButton = document.querySelector(".container-1 .name");
+  dogButton.addEventListener("click", () => {
+    alert("dog s");
+  });
+});
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  const catButton = document.querySelector(".container-2 .name");
+  catButton.addEventListener("click", () => {
+    const boxContainer = document.querySelector(".box-container");
+    boxContainer.style.backgroundColor = "green";
+  });
+});
